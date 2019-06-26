534d80e820a603d2f3fb396445dbf50ad210f81d
Jennifer Taylor <dragonminded@dragonminded.com>
2019-06-26 22:53:00 +0000

    Add ability to patch a binary or exe using a previously generated patchlist.

--- utils/binary.py
+++ utils/binary.py
@@ -66,3 +66,78 @@ class Binary:
 
         # Return our summation
         return ret
+
+    @staticmethod
+    def patch(
+        binary: bytes,
+        patches: List[str],
+        *,
+        reverse: bool = False,
+    ) -> bytes:
+        # First, separate out into a list of offsets and old/new bytes
+        differences: List[Tuple[int, bytes, bytes]] = []
+
+        for patch in patches:
+            start_offset, patch_contents = patch.split(':', 1)
+            before, after = patch_contents.split('->')
+            beforevals = [
+                int(x.strip(), 16) for x in before.split(" ") if x.strip()
+            ]
+            aftervals = [
+                int(x.strip(), 16) for x in after.split(" ") if x.strip()
+            ]
+
+            if len(beforevals) != len(aftervals):
+                raise Exception(
+                    f"Patch before and after length mismatch at "
+                    f"offset {start_offset}!"
+                )
+            if len(beforevals) == 0:
+                raise Exception(
+                    f"Must have at least one byte to change at "
+                    "offset {start_offset}!"
+                )
+
+            offset = int(start_offset.strip(), 16)
+            beforebytes = bytes(beforevals)
+            afterbytes = bytes(aftervals)
+
+            for i in range(len(beforebytes)):
+                differences.append(
+                    (
+                        offset + i,
+                        beforebytes[i:i+1],
+                        afterbytes[i:i+1],
+                    )
+                )
+
+        # Now, if we're doing the reverse, just switch them
+        if reverse:
+            differences = [(x[0], x[2], x[1]) for x in differences]
+
+        def _hex(val: int) -> str:
+            out = hex(val)[2:]
+            out = out.upper()
+            if len(out) == 1:
+                out = "0" + out
+            return out
+
+        # Now, apply the changes to the binary data
+        for diff in differences:
+            offset, old, new = diff
+
+            if len(binary) < offset:
+                raise Exception(
+                    f"Patch offset {_hex(offset)} is beyond the end of "
+                    f"the binary!"
+                )
+            if binary[offset:(offset+1)] != old:
+                raise Exception(
+                    f"Patch offset {_hex(offset)} expecting {_hex(old[0])} "
+                    f"but found {_hex(binary[offset])}!"
+                )
+
+            binary = binary[:offset] + new + binary[(offset + 1):]
+
+        # Return the new data!
+        return binary
